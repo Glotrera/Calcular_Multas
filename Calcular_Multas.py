b201fe1851bb1ca47a78e748fbf61f383d58c9bb
FranciscoC <franciscolcpereira99@gmail.com>
2025-07-23 12:46:39 +0100

    Update Calcular_Multas.py
    Multas Localidade

--- Calcular_Multas.py
+++ Calcular_Multas.py
@@ -12,5 +12,11 @@ vel = int(input('Introduza a velocidade: '))
 if vel <= 50:
     print('Não há multa a pagar.')
     
-
+elif loc == 1:
+    if vel > 50 and vel < 90:
+        print('A multa a pagar é de 60 euros.')
+    elif vel >= 90 and vel < 120:
+        print('A multa a pagar é de 120 euros.')
+    elif vel >= 120:
+        print('A multa a pagar é de 320 euros.')
 
